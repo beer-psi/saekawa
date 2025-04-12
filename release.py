62e35ec359c4b19874b6925471e68bc8c0eaf7b2
beer-psi <lacvtg.a1.2023@gmail.com>
2025-04-13 01:19:21 +0700

    add stuff for patafour

--- release.py
+++ release.py
@@ -125,6 +125,34 @@ rainycolor_watercolor_manifest = {
 with (RAINYCOLOR_WATERCOLOR_FOLDER / "manifest.json").open("w", encoding="utf-8") as f:
     json.dump(rainycolor_watercolor_manifest, f, ensure_ascii=False, indent=4)
 
+rainycolor_watercolor_readme = """<h1 align="center">saekawa</h1>
+
+<p align="center">インパアフェクシオン・ホワイトガアル</p>
+
+CHUNITHM hook to submit your scores to Tachi every credit.
+
+### Features
+- Submit scores to Tachi after each credit.
+- Submit dan and emblem classes to Tachi.
+
+### Usage
+Download a config file pre-filled with your Tachi API key [here](https://kamai.tachi.ac/client-file-flow/CXSaekawa)
+and point STARTLINER to your config file.
+
+Scores are sent after every credit. If score submission is taking a long time, please don't close the game just yet.
+You can monitor that the hook is working through the console, or through the `saekawa.log` log file.
+
+### Credits
+- Adam Thibert ([adamaq01](https://github.com/adamaq01)). A lot of the code was copied from
+[Mikado](https://github.com/adamaq01/Mikado), a similar hook for SDVX.
+
+### License
+0BSD
+"""
+with (RAINYCOLOR_WATERCOLOR_FOLDER / "README.md").open("w", encoding="utf-8") as f:
+    _ = f.write(rainycolor_watercolor_readme)
+
+_ = shutil.copy2("res/icon.png", RAINYCOLOR_WATERCOLOR_FOLDER / "icon.png")
 _ = shutil.copy2(
     COMPILED_OUTPUT, RAINYCOLOR_WATERCOLOR_FOLDER / "app" / "bin" / "saekawa.dll"
 )
